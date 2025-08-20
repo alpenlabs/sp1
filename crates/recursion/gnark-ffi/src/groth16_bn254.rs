afb0bb01d6991c88b490541149bb5ef5b5f70cb8
manishbista28 <manishbista28@gmail.com>
2025-08-20 10:55:02 +0545

    fix: avoid build_contracts in build fn

--- crates/recursion/gnark-ffi/src/groth16_bn254.rs
+++ crates/recursion/gnark-ffi/src/groth16_bn254.rs
@@ -86,7 +86,7 @@ impl Groth16Bn254Prover {
         build_groth16_bn254(build_dir.to_str().unwrap());
 
         // Build the contracts.
-        Self::build_contracts(build_dir);
+        // Self::build_contracts(build_dir);
     }
 
     /// Generates a Groth16 proof given a witness.
