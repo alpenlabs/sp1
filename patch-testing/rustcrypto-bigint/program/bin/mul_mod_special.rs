a8ed39060acf75deb355391afac20144a82cb562
manishbista28 <manishbista28@gmail.com>
2025-08-27 13:56:41 +0545

    compilation fixes

--- patch-testing/rustcrypto-bigint/program/bin/mul_mod_special.rs
+++ patch-testing/rustcrypto-bigint/program/bin/mul_mod_special.rs
@@ -9,11 +9,13 @@ pub fn main() {
     for _ in 0..times {
         let a: [u32; 8] = sp1_lib::io::read::<Vec<u32>>().try_into().unwrap();
         let b: [u32; 8] = sp1_lib::io::read::<Vec<u32>>().try_into().unwrap();
+        let a: [u64; 8] = a.map(|x| x as u64);
+        let b: [u64; 8] = b.map(|x| x as u64);
         let a = Uint::<8>::from_words(a);
         let b = Uint::<8>::from_words(b);
 
         let c: u32 = 356u32;
-        let c = Limb(c);
+        let c = Limb(c.into());
         let result = a.mul_mod_special(&b, c);
 
         sp1_lib::io::commit(&result.to_be_bytes().to_vec());
